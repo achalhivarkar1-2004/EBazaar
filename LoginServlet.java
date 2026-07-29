b083b117e1678c466e293f2699fc6748d4a4e9af
Achal Hiwarkar <achalhivarkar1@gmail.com>
2026-07-29 22:49:05 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,49 @@
+package com.ebazar.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String user = request.getParameter("username");
+		String pass = request.getParameter("password");
+		
+		if(user.equals("achal") && pass.equals("achal")) {
+			
+			response.sendRedirect("mobiles.jsp");
+		} else {
+			response.getWriter().println("<h2>Invalid Username or Password</h2>");
+		}
+	}
+
+}
---
+++ MobileController.java
@@ -0,0 +1,82 @@
+package com.ebazar.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+/**
+ * Servlet implementation class MobileController
+ */
+@WebServlet("/mobilecontroller")
+public class MobileController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MobileController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		// Get Selected Product
+
+		String product =
+		request.getParameter("product");
+
+		// Create Session
+
+		HttpSession session =
+		request.getSession();
+
+		// Get Cart Data
+
+		ArrayList<String> cart = (ArrayList<String>) session.getAttribute("cart");
+
+		// If Cart Empty
+
+		if(cart == null){
+
+			cart = new ArrayList<String>();
+		}
+
+		// Add Product To Cart
+
+		if(product != null){
+
+			cart.add(product);
+		}
+
+		// Save Cart In Session
+
+		session.setAttribute("cart", cart);
+
+		// Redirect To Cart Page
+
+		response.sendRedirect("cart.jsp");
+
+
+	}
+		}
+
